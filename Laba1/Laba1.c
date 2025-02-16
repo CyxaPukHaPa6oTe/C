4ca7afad78addffb9536694ed4d390a8ff7cdcdb
CyxaPukHaPa6oTe <cyxapukhapa6ote@gmail.com>
2025-02-16 15:00:41 +1000

    Add files via upload
    New version

--- Laba1/Laba1.c
+++ Laba1/Laba1.c
@@ -2,29 +2,43 @@
 #include <math.h>
 
 int main(int argc, char** argv) {
-	double a,b,c;
-	double x1,x2,d;
-	
-	a = 1.2;
-	b = 10;
-	c = 3.3;
-	
-	d = pow(b,2) - 4*a*c;
-	printf("d - %1f \n",d);
-	
-	if (d>0){
-	    x1 = -b + sqrt(d)/(2*a);
-	    x2 = -b - sqrt(d)/(2*a);
-	    printf("x1 - %1f \n", x1);
-	    printf("x2 - %1f \n", x2);
-	}
-	
-	if (d==0){
-	    x1 = -b + sqrt(d)/(2*a);
-	    printf("x - %1f \n", x1);
-	}
-	
-	if (d<0) printf("Korney net");
-	
-	return 0;
-}
+    double a, b, c;
+    double x1, x2, d;
+    char Vibor;
+/*
+    a = 1.2
+    b = 10
+    c = 3.3
+*/
+
+    do {
+        printf("Vvedite A: ");
+        scanf("%lf", &a);
+        printf("Vvedite B: ");
+        scanf("%lf", &b);
+        printf("Vvedite C: ");
+        scanf("%lf", &c);
+
+        d = pow(b, 2) - 4 * a * c;
+        printf("d - %lf \n", d);
+
+        if (d > 0) {
+            x1 = (-b + sqrt(d)) / (2 * a);
+            x2 = (-b - sqrt(d)) / (2 * a);
+            printf("x1 - %lf \n", x1);
+            printf("x2 - %lf \n", x2);
+        }
+        else if (d == 0) {
+            x1 = (-b + sqrt(d)) / (2 * a);
+            printf("x - %lf \n", x1);
+        }
+        else if (d < 0) {
+            printf("Korney net\n");
+        }
+
+        printf("Hotite Restart prog? (W/Another letter): ");
+        scanf(" %c", &Vibor);
+
+    } while (Vibor == 'W' || Vibor == 'w');
+
+    return 0;}
